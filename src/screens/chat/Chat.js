7324256b5f6c93f564c816a13fdbb40113dd99fa
santanugh22 <11463046+santanugh22@users.noreply.github.com>
2024-06-26 13:03:19 +0530

    Added many things

--- src/screens/chat/Chat.js
+++ src/screens/chat/Chat.js
@@ -2,11 +2,13 @@ import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 const Chat = () => {
   const insets = useSafeAreaInsets();
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
         paddingTop: Platform.OS === "android" ? 2 * insets.top : null,
+        backgroundColor: "#000",
       }}
     ></SafeAreaView>
   );
