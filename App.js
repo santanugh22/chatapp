37280ae101a52892b2c4780f97684d87f175ad22
santanugh22 <11463046+santanugh22@users.noreply.github.com>
2024-06-26 12:14:47 +0530

    added expo-sqlite and other packages

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
+
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./src/navigators/StackNavigator";
 
--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,12 +1,53 @@
 import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import LoginHeader from "../../components/login/LoginHeader";
 import AxiosContext from "../../utils/AxiosContext";
 
-const Login = () => {
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Login = ({ navigation }) => {
   const insets = useSafeAreaInsets();
   const axiosInstance = useContext(AxiosContext);
+  // const navigation = useNavigation();
+  const [error, setError] = useState(null);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  async function Login() {
+    try {
+      const result = await axiosInstance.post("auth/login", {
+        email,
+        password,
+      });
+      const { token, username } = result.data;
+      await AsyncStorage.setItem("token", token);
+      await AsyncStorage.setItem("username", username);
+    } catch (error) {
+      setError(error.response.data);
+    }
+  }
+
+  async function CheckLoginStatus() {
+    try {
+      const token = await AsyncStorage.getItem("token");
+      if (token) {
+        setLoggedIn(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    CheckLoginStatus();
+    if (loggedIn) {
+      navigation.replace("CHAT");
+    }
+  }, []);
+
   return (
     <SafeAreaView
       style={{
